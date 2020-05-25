ce596d698b6daf600d6ef42cdedcfb072745d7d8
yuvanseth <55777826+yuvanseth@users.noreply.github.com>
2020-05-26 01:47:51 +0530

    Update e-mail.py

--- e-mail.py
+++ e-mail.py
@@ -1,17 +1,49 @@
+import imaplib
 import email
+import os
+from lib.eml2html import EmailtoHtml
+from lib.html2image import HtmltoImage
 
-from imapclient import IMAPClient
+EMAIL_ADDRESS = 'yseth27@gmail.com'
+EMAIL_PSWD = 'ybjbkfmzhxfwytdn'
+EMAIL_MAILBOX = 'INBOX'
+IMAP_SERVER = 'imap.gmail.com'
 
-HOST = 'imap.gmail.com'
-USERNAME = 'yseth27@gmail.com'
-PASSWORD = 'mypassword'
 
-with IMAPClient(HOST) as client:
-    client.login(USERNAME, PASSWORD)
-    client.select_folder('INBOX', readonly=True)
+class EmailHelper(object):
+    def __init__(self, IMAP_SERVER, EMAIL_ADDRESS,
+                 EMAIL_PSWD, EMAIL_MAILBOX):
+        # logs in to the desired account and navigates to the inbox
+        self.mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+        self.mail.login(EMAIL_ADDRESS, EMAIL_PSWD)
+        self.mail.select()
 
-    messages = client.search('ALL')
-    for i in range (10):
-        uid, message_data = client.fetch(messages, 'RFC822').items()
-        email_message = email.message_from_bytes(message_data[b'RFC822'])
-        print(uid, email_message.get('From'), email_message.get('Subject'))
+    def get_emails(self):
+        uids = self.mail.uid('SEARCH', 'ALL')[1][0].split()
+        return uids
+
+    def get_email_message(self, email_id):
+        _, data = self.mail.uid('FETCH', email_id, '(RFC822)')
+        raw_email = data[0][1]
+        raw_email_string = raw_email.decode('utf-8')
+        email_message = email.message_from_string(raw_email_string)
+        return email_message
+
+
+email_helper = EmailHelper(IMAP_SERVER, EMAIL_ADDRESS,
+                           EMAIL_PSWD, EMAIL_MAILBOX)
+email_to_html_convertor = EmailtoHtml()
+html_to_image_convertor = HtmltoImage()
+uids = email_helper.get_emails()
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+output_dir = os.path.join(dir_path, "images")
+
+for uid in uids[:10]:
+    email_message = email_helper.get_email_message(uid)
+    html = email_to_html_convertor.convert(email_message)
+    filename = uid.decode() + ".jpg"
+    pdf_path = html_to_image_convertor.save_image(
+        html.encode(), output_dir, filename)
+    print(pdf_path)
+
